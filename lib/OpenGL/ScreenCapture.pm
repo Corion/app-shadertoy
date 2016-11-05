ccfbbe07c9c78ffa59c864322b76c775630dd89f
Max Maischein <corion@corion.net>
2016-11-05 20:42:57 +0100

    Make Imager an optional method to output/save data

--- lib/OpenGL/ScreenCapture.pm
+++ lib/OpenGL/ScreenCapture.pm
@@ -1,5 +1,6 @@
 package OpenGL::ScreenCapture;
 use strict;
+use Carp 'croak';
 use Exporter 'import';
 use vars qw($VERSION @EXPORT_OK);
 use OpenGL::Glew qw(glReadPixels glReadBuffer glGetIntegerv
@@ -11,7 +12,6 @@ use OpenGL::Glew qw(glReadPixels glReadBuffer glGetIntegerv
     glPixelStorei
 );
 use OpenGL::Glew::Helpers qw(xs_buffer);
-use Prima;
 
 use Filter::signatures;
 use feature 'signatures';
@@ -26,7 +26,17 @@ no warnings 'experimental::signatures';
     my $image = capture();
     $image->save('screenshot.png');
 
-Returns the current OpenGL buffer as an L<Prima> image.
+Returns the current OpenGL buffer as an L<Prima> image. This behaviour
+is the default. If your program otherwise doesn't use Prima, you can pass
+C<Imager> as an option to get an L<Imager> object back:
+
+    my $image = capture(format => 'Imager');
+    $image->write(filename => 'screenshot.png');
+
+Note that the method names are different between Imager and Prima!
+
+L<Imager> is not a prerequisite of this module and thus is not automatically
+installed.
 
 =cut
 
@@ -34,7 +44,9 @@ sub capture(%options) {
     $options{x} ||= 0;
     $options{y} ||= 0;
     $options{type} ||= GL_UNSIGNED_BYTE();
-    
+
+    $options{ format } ||= 'Prima';
+
     if( not exists $options{ width } or not exists $options{height} ) {
         glGetIntegerv( GL_VIEWPORT, xs_buffer(my $viewport, 32 ));
         my($x,$y,$w,$h) = unpack 'IIII', $viewport;
@@ -55,22 +67,47 @@ sub capture(%options) {
         $options{type},
         xs_buffer(my $buffer, $options{width}*$options{height}*4),
     );
-    print glGetError,"\n";
 
-    return Prima::Image->new(
-        width    => $options{width},
-        height   => $options{height},
-        type     => im::Color | im::bpp32 | im::fmtBGRI,
-	lineSize => $options{width}*4,
-	data     => $buffer,
-    );
+    if( $options{ format } eq 'Prima' ) {
+        require Prima;
+        return Prima::Image->new(
+            width    => $options{width},
+            height   => $options{height},
+            type     => im::Color | im::bpp32 | im::fmtBGRI,
+            lineSize => $options{width}*4,
+            data     => $buffer,
+        );
+
+    } elsif( $options{ format } eq 'Imager' ) {
+        require Imager;
+		my $i = Imager->new(
+			xsize => $options{width},
+			ysize => $options{height},
+			type => 'direct',
+			bits => 8, # per channel
+			filetype => 'raw',
+		);
+		$i->read(
+			data => $buffer,
+			type => 'raw',
+			xsize => $options{width},
+			ysize => $options{height},
+			raw_datachannels => 4,
+			raw_interleave => 0,
+		);
+		$i->flip(dir => 'v');
+		return $i
+
+    } else {
+        croak "Unknown object format option '$options{format}', need 'Prima' or 'Imager'";
+    };
 }
 
 1;
 
 =head1 REPOSITORY
 
-The public repository of this module is 
+The public repository of this module is
 L<http://github.com/Corion/app-shadertoy>.
 
 =head1 SUPPORT
