225ba21a8cd539a4871cf2bd0deb363dc87d0ed0
Max Maischein <corion@corion.net>
2016-11-06 19:09:17 +0100

    Use OpenGL 4-style texture loading when available
    This is the much more typesafe method. Once I had found this one in the
    list of common OpenGL mistakes [0], fixing
    my code to work with OpenGL 3 was much easier as the page
    explained why the new code was the better approach.
    
    [0] https://www.opengl.org/wiki/Common_Mistakes#Creating_a_complete_texture

--- lib/OpenGL/Texture.pm
+++ lib/OpenGL/Texture.pm
@@ -6,21 +6,27 @@ use OpenGL::Glew qw(
     glActiveTexture
     glBindTexture
     glTexImage2D
+    glTexSubImage2D
+    glTexStorage2D
     GL_TEXTURE_2D
 	GL_TEXTURE
     GL_TEXTURE0
+    GL_RGBA8
     GL_RGBA
     GL_UNSIGNED_BYTE
     
     glObjectLabel
+	glTexParameteri
+	GL_TEXTURE_BASE_LEVEL
+	GL_TEXTURE_MAX_LEVEL
 );
-use OpenGL::Glew::Helpers (qw(xs_buffer croak_on_gl_error));
+use OpenGL::Glew::Helpers (qw(xs_buffer croak_on_gl_error glGetVersion_p));
 
 use Filter::signatures;
 use feature 'signatures';
 no warnings 'experimental::signatures';
 
-use vars '$VERSION';
+use vars qw($VERSION $glVersion);
 $VERSION = '0.01';
 
 # This should also use Prima as default instead of Imager, at least
@@ -48,7 +54,7 @@ sub load($class,$filename,%options) {
 }
 
 sub new($class,%options) {
-    $options{ target_format } ||= GL_RGBA;
+    $options{ target_format } ||= GL_RGBA8;
     $options{ source_format } ||= GL_RGBA;
     
     bless \%options => $class;
@@ -74,21 +80,49 @@ As a side effect, C<GL_TEXTURE0> is unbound.
 =cut
 
 sub store($self,%options) {
-    glActiveTexture(GL_TEXTURE0);
     my $id = $self->id;
-    glBindTexture(GL_TEXTURE_2D,$id);
 	my $buf = ${$options{data}} . '    '; # padding to prevent segfaults by the OpenGL API tracer...
-    glTexImage2D(GL_TEXTURE_2D,
-                 0,
-                 $options{ target_format } || $self->{target_format},
-                 $options{ width },
-                 $options{ height },
-                 0,
-                 $options{ source_format } || $self->{source_format },
-                 GL_UNSIGNED_BYTE,
-                 $buf
-    );
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D,$id);
+	
+    $glVersion ||= glGetVersion_p;
+
+	if($glVersion >= 4.2) {
+		# This is OpenGL 4.2 only but much more typesafe:
+		glTexStorage2D(GL_TEXTURE_2D,
+		               1,
+					   $options{ target_format } || $self->{target_format},
+					   $options{width},
+					   $options{height}
+		);
+		glTexSubImage2D(GL_TEXTURE_2D,
+					 0,
+					 0,
+					 0,
+					 $options{ width },
+					 $options{ height },
+					 $options{ source_format } || $self->{source_format },
+					 GL_UNSIGNED_BYTE,
+					 $buf
+		);
+
+    } else {		
+		# OpenGL 1.2 to OpenGL 3
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+		glTexImage2D(GL_TEXTURE_2D,
+					 0,
+					 $options{ target_format } || $self->{target_format},
+					 $options{ width },
+					 $options{ height },
+					 0,
+					 $options{ source_format } || $self->{source_format },
+					 GL_UNSIGNED_BYTE,
+					 $buf
+		);
+	};
     
+	# This should also only be done if the GL version is high enough
     if( $options{ name }) {
         glObjectLabel(GL_TEXTURE,$id, length $options{name},$options{name});
     };
